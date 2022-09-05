b5efd7cbf7806fa02a1e0e1fba157cefbc6bea8d
erikvs <89622099+erikvs@users.noreply.github.com>
2022-09-05 16:51:48 +0200

    Task 4 Complete

--- JavaScript_Intro_Oppgaver-main/Oppgave_04/src/mest_hjelp.js
+++ JavaScript_Intro_Oppgaver-main/Oppgave_04/src/mest_hjelp.js
@@ -1,41 +1,43 @@
-import './styles.css';
+// Getting placement for numbers to guess.
+const numbersUl = document.getElementById('numbers');
+// Getting placement for guesses.
+const guessUl = document.getElementById('guess');
+// Getting button to submit guesses.
+const button = document.querySelector('button');
+// List of numbers to sort.
+const numbers = [1, 3, 2];
 
-// TODO: Bruk getElementById til å hente HTML med #id
-// TODO: Bruk querySelector til å hente knappen
-const numbersUl = '';
-const guessUl = '';
-const button = '';
-// TODO: Lag en liste med tallene som skal sorteres
-const numbers = [];
-
+// Getting all guesses.
 const getGuess = () => {
-  // TODO: Bruk querySelectorAll på guessUl for å hente ut alle input feltene
-  const answers = '';
-  // TODO: Bruk .map for å hente ut verdiene i input feltene
-  return Array.from(answers).map();
+  const answers = document.querySelectorAll('input');
+  return Array.from(answers).map((answer) => answer.value);
 };
 
+// Checking that guesses sequence is correct or false.
 const checkNumberSeq = () => {
   const guess = getGuess();
   let isCorrect = numbers.sort().join('') === guess.join('');
   if (isCorrect) {
     alert('Du sorterte riktig');
   }
+  if (!isCorrect){
+    alert('Du sorterte feil');
+  }
 };
 
+// Function for adding fields to guess input.
 const addInputUI = () => {
-  // TODO: Bruk for-of (eller vanlig for-løkke) og guessUl til å lage li-elementer med input elementer for å kunne skrive inn hva som skal sorteres
-  for (let _ of _) {
-    const li = ``;
+  for (let num of numbers) {
+    const li = `<li><input></input></li>`;
     guessUl.innerHTML += li;
   }
 };
 
+// Function for presenting the list of numbers to guess order of.
 const addNumbersUI = () => {
-  // TODO: Bruk for-of (eller vanlig for-løkke) og numbersUl til å lage li-elementer med tallene som skal sorteres
-  for (let _ of _) {
-    const li = ``;
-    numbersUl.innerHTML += li;
+  for (let num of numbers) {
+    const li = num;
+    numbersUl.innerHTML += `<li>${li}</li>`;
   }
 };
 
