be33472dae99bb8c45d531394caf4d85173859b4
erikvs <89622099+erikvs@users.noreply.github.com>
2022-09-25 19:46:21 +0200

    task 2
    Completed

---
+++ Intro_React/src/components/myComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+//TASK 2
+export default function MyComponent({ title }) {
+  return <h1>{title}</h1>;
+}
+
+
+//TASK 1
+// const MyComponent = () => <h1>My First Component</h1>;
+// export default MyComponent;
