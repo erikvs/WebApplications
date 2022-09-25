f79008a1ff2568e3e9fef0f45bec1b716d4d98e6
erikvs <89622099+erikvs@users.noreply.github.com>
2022-09-25 16:48:38 +0200

    INIT
    Initial commit for Intro to React

---
+++ Intro_React/src/App.js
@@ -0,0 +1,10 @@
+import { myComponent } from './components/myComponent';
+
+const App = () => (
+  <div>
+    <p>Read the README.md to see the tasks</p>
+  </div>
+);
+
+
+export default App;
---
+++ Intro_React/src/components/myComponent.js
@@ -0,0 +1,3 @@
+export default function MyComponent() {
+  return <h1>My First Component</h1>;
+}
