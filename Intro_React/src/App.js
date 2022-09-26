fff0f2175d03ea432bbe1d053edf0f4ff5ecca87
erikvs <89622099+erikvs@users.noreply.github.com>
2022-09-26 18:09:16 +0200

    Intro React
    modules installed
    first task running

---
+++ Intro_React/src/App.js
@@ -0,0 +1,14 @@
+import MyComponent from "./components/myComponent";
+
+//{title}
+
+const App = () => (
+<MyComponent />
+
+);
+
+
+export default App;
+
+
+{/* <MyComponent title="It Works" /> */}
---
+++ Intro_React/src/components/myComponent.js
@@ -0,0 +1,14 @@
+// const MyComponent = ({title}) => {
+//     return <h1>{title}</h1>;
+// }
+
+
+//TASK 2
+// const MyComponent = ({title}) => 
+// <h1>{title}</h1>;
+
+//TASK 1
+const MyComponent = () => <h1>My First Component</h1>;
+
+
+export default MyComponent;
