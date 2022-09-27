c7055b7b12378516a6f56f18f8361aad1cc0d62f
erikvs <89622099+erikvs@users.noreply.github.com>
2022-09-27 13:19:19 +0200

    React Intro 10

--- Intro_React/src/components/inputField.js
+++ Intro_React/src/components/inputField.js
@@ -1,12 +1,25 @@
-// TASK 9
+// TASK 9 + 10
+
+// import hook to use
+import {useState} from "react"
 
 const InputField = () => {
-    const handleChange = () => {
+
+    // ready hook use, default value set
+    const [value, setValue] = useState("Initial Value")
+
+    const handleChange = (e) => {
+
+        // updates value with user input
+        setValue(e.target.value);
+
         console.log("change");
+        //console.log(value)
     };
 return (
+    // added "value={state}" to input field to make it controlled
     <>
-    <input type="text" onChange={handleChange}/>
+    <input type="text" onChange={handleChange} value={value}/>
     </>
 );
 };
